8e5d9917d241cf2ed3025a119a79a35b6332d00b
Somya Sarathi Samal <soms9edu@gmail.com>
2021-10-06 13:56:52 +0530

    Added a brute force approach to the problem 1.9 String Rotation
    Data Structure Used:
    strings
    
    Time Complexity:
    O(n)
    
    File Added: 0
    
    File Modified: +1
    09_string_rotation

--- 01_array_n_strings/09_string_rotation.py
+++ 01_array_n_strings/09_string_rotation.py
@@ -10,10 +10,28 @@ def isSubstring_dp(s1, s2, memo, n):
 
     return isSubstring_dp(shiftRight(s1), s2, memo, n+1)
 
+
+def isSubstring(s1, s2):
+
+    if s1 == s2:
+        return True
+
+    for i in range(len(s2)):
+        s1 = shiftRight(s1)
+
+        if s1 == s2:
+            return True
+
+    return False
+
+
 if __name__ == "__main__":
     s1, s2 = input(), input()
-    
-    if isSubstring_dp(s1, s2, s1, 1):
-        print("Yes")
+
+    if len(s1) == len(s2):
+        if isSubstring_dp(s1, s2, s1, 1):
+            print("Yes")
+        else:
+            print("No")
     else:
-        print("Yes")
+        print("No")
