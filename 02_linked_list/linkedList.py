a7173854f68b2191d89a654e6ba513cad989a70c
Somya Sarathi Samal <soms9edu@gmail.com>
2021-10-07 18:27:31 +0530

    merged branch linkedlist with main branch

--- 02_linked_list/linkedList.py
+++ 02_linked_list/linkedList.py
@@ -4,104 +4,90 @@ class Node:
         self.next = next
 
 class LinkedList:
-    """
-        mutable sequence\n
-        if no value given creates an new empty Linked List
-    """
     length = 0
+    tail = None
+    head = None
+    string = ''
 
     def __init__(self, head=None):
-        self.head = head
+
+        if head != None:
+            self.head = Node(head)
+        
+        if self.head != None:
+            self.tail = self.head
+            self.string += '→ ' + str(head)
+            self.length += 1
+            return
 
 
     def __str__(self):
-        if self.head is None:
-            return "Empty"
-
-        itr = self.head
-
-        llstr = ''
-
-        while itr:
-            llstr +='→ ' + str(itr.data) + ' '
-            itr = itr.next
-
-        return llstr
-
-
-    def len(self):
-        """
-            Returns the length of the Linked List
-        """
-        return self.length
-
-
-    def unShift(self, data):
-        """
-            1. Initializes a head to an empty Linked List
-            2. Appends data at the beginning of the Linked List
-        """
-        newNode = Node(data, self.head)
-        self.head = newNode
-        self.length += 1
-
-
-    def shift(self):
-        """
-            1. Removes the first value in a Linked List
-            2. Returns the removed value
-        """
-        data = self.head.data
-        self.head = self.head.next
-
-        return data
+        return self.string
 
 
     def push(self, data):
-        """
-            Inserts value to the end of Linked List
-        """
-
-        if self.head is None:
+        if self.head == None:
             self.head = Node(data)
-            return data
+            self.tail = self.head
+            self.string += '→ ' + str(data)
+            self.length += 1
+            return
 
-        itr = self.head
+        self.tail.next = Node(data)
+        self.tail = self.tail.next
 
-        while itr.next:
-            itr = itr.next
-
-        itr.next = Node(data)
+        self.string += ' → ' + str(data)
         self.length += 1
+
+        return
 
 
     def pop(self):
-        """
-            1. Removes the last value from the Linked List
-            2. Return the removed value
-        """
         if self.head == None:
             return
 
         itr = self.head
 
         while itr:
-            if (itr.next).next == None:
+            if itr.next.next == None:
+                self.tail = itr
                 break
             itr = itr.next
 
         data = itr.next.data
         itr.next = None
+
+        self.string = self.string[0 : -len(str(data)) - 3]
         self.length -= 1
 
         return data
 
+    
+    def unshift(self, data):
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = self.head
+            self.string += '→ ' + str(data)
+            self.length += 1
+            return
 
-    def append(self, data):
-        """
-            Appends the list of data at the end of Linked List
-        """
-        if type(data) is not list:
-            return "Expected a list"
-        for x in data:
-            self.push(x)
+        newHead = Node(data, self.head)
+        self.head = newHead
+
+        self.string = f'→ {str(data)} {self.string}'
+        self.length += 1
+
+        return
+
+    def shift(self):
+        if self.head == None:
+            return
+        
+        data = self.head.data
+        newHead = self.head.next
+        self.head = newHead
+
+        self.string = self.string[len(str(data)) + 3 : ]
+        self.length -= 1
+
+        return
