56b46e3b31b249bc65d38ee8843aeeca66044607
Somya Sarathi Samal <soms9edu@gmail.com>
2021-10-12 19:37:14 +0530

    Updates singly module
    - added few comparision special methods to the Node and Singly class
    - added two new methods to Singlly class shift() and unshift()

--- 02_linked_list/singly.py
+++ 02_linked_list/singly.py
@@ -1,35 +1,84 @@
 class Node:
-    def __init__(self, data, next=None) -> None:
+    def __init__(self, data=None, nxt=None):
         self.data = data
-        self.next = next
-        return
-
-    def __del__(self) -> object:
-        return self.data
-
-
-class singly:
-    head = None
-    tail = None
-    length = 0
-    llist = []
-
-    def __init__(self) -> None:
-        return
-
-
-    def __del__(self) -> None:
-        return
-
-
-    def __len__(self) -> int:
-        return self.length
+        self.nxt = nxt
 
 
     def __str__(self) -> str:
-        return  '-> ' + ' -> '.join(map(str, self.llist))
+        return str(self.data)
 
 
+    def __repr__(self) -> str:
+        return f"'{self.__str__()}'"
+
+
+    def __eq__(self, o: object) -> bool:
+        if self.data == o:
+            return True
+
+        return False
+
+
+    def __gt__(self, o: object) -> bool:
+        if self.data > o.data:
+            return True
+
+        return False
+
+    
+    def __ge__(self, o: object) -> bool:
+        if self.data >= o.data:
+            return True
+
+        return False
+
+
+    def __lt__(self, o: object) -> bool:
+        if self.data < o.data:
+            return True
+
+        return False
+
+
+    def __le__(self, o: object) -> bool:
+        if self.data <= o.data:
+            return True
+
+        return False
+
+
+class Singly:
+    head = Node()
+    tail = None
+    length = 0
+
+    def __init__(self, data=None) -> None:
+        self.llist = []
+
+        if data == None:
+            return
+
+        if type(data) == list or type(data) == tuple:
+            for x in data:
+                self.push(x)
+
+            return
+
+        self.head = Node(data)
+        self.tail = self.head
+        self.llist.append(data)
+        self.length += 1
+        return
+
+
+    def __str__(self) -> str:
+        return '→ ' + ' → '.join(map(str, self.llist))
+
+
+    def __repr__(self) -> str:
+        return f"'{self.__str__()}'"
+
+    
     def __eq__(self, o: object) -> bool:
         if self.llist == o.llist:
             return True
@@ -38,34 +87,34 @@ class singly:
 
 
     def __gt__(self, o: object) -> bool:
-        if len(self) > len(o):
-            return True
-
-        return False
-
-
-    def __lt__(self, o: object) -> bool:
-        if len(self) < len(o):
+        if self.llist > o.llist:
             return True
 
         return False
 
     
     def __ge__(self, o: object) -> bool:
-        if len(self) >= len(o):
+        if self.llist >= o.llist:
+            return True
+
+        return False
+
+
+    def __lt__(self, o: object) -> bool:
+        if self.llist < o.llist:
             return True
 
         return False
 
 
     def __le__(self, o: object) -> bool:
-        if len(self) <= len(o):
+        if self.llist <= o.llist:
             return True
 
         return False
 
 
-    def push(self, data: object) -> None:
+    def push(self, data):
         if self.head == None:
             self.head = Node(data)
             self.tail = self.head
@@ -74,25 +123,45 @@ class singly:
             return
 
         node = Node(data)
-        self.tail.next = node
-        self.tail = self.tail.next
+        self.tail.nxt = node
+        self.tail = self.tail.nxt
         self.llist.append(data)
         self.length += 1
         return
 
 
-    def pop(self) -> object:
+    def pop(self):
         if self.head == None:
             return
 
         itr = self.head
-        toDelete = self.tail
-        while itr.next.next != None:
-            itr = itr.next
+        while itr.nxt.nxt != None:
+            itr = itr.nxt
 
-        itr.next = None
+        data = itr.data
         self.tail = itr
-        data = self.llist.pop()
+        self.tail.nxt = None
+
+        self.llist.pop()
         self.length -= 1
-        del toDelete
+
+        return data
+
+
+    def unshift(self, data) -> None:
+        node = Node(data, self.head)
+        self.head = node
+        self.llist.insert(0, data)
+        self.length += 1
+
+
+    def shift(self):
+        if self.head == None:
+            return
+
+        data = self.head.data
+        self.head = self.head.nxt
+        self.llist = self.llist[1:]
+        self.length -= 1
+
         return data
