0eb884b763ce7e734c474c832ec5e63a7d6fe306
Somya Sarathi Samal <73218471+somyaSarathi@users.noreply.github.com>
2021-11-12 17:05:57 +0530

    Created Graph class using hash map implementation
    - File Added: 0
    
    - File Modified: +1
        ./04_trees_and_graphs/graph.py

--- 04_trees_and_graphs/graph.py
+++ 04_trees_and_graphs/graph.py
@@ -1,17 +1,48 @@
-class vertex:
-  def __init__(self, val, *edges):
-    if val == None:
-      raise TypeError
-    
-    self.data = val
-    self.edges = []
-    
-    if edges:
-      for edge in edges:
-        self.edges.append(edge)
+class graph:
+    def __init__(self) -> None:
+        self.vertices: dict = dict()
+        self.weights: dict = dict()
+        return
+
+    def addVertex(self, vertex, edges: list=[], weights: list=[]) -> None:
+        if not edges:
+            try:
+                self.vertices[vertex]
+            except KeyError:
+                self.vertices[vertex] = set()
+            return
+
+        if edges and weights:
+            if len(edges) < len(weights):
+                raise ValueError
+
+        try:
+            for i in range(len(edges)):
+                self.vertices[vertex].add(edges[i])
+                try:
+                    self.weights[(vertex, edges[i])] = weights[i]
+                    self.addVertex(edges[i])
+                except IndexError:
+                    self.weights[(vertex, edges[i])] = 1
+
+        except KeyError:
+            self.vertices[vertex] = set()
+            for i in range(len(edges)):
+                self.vertices[vertex].add(edges[i])
+                try:
+                    self.weights[(vertex, edges[i])] = weights[i]
+                    self.addVertex(edges[i])
+                except IndexError:
+                    self.weights[(vertex, edges[i])] = 1
         
-    return
-  
-  
-  def __str__(self):
-    return self.val.__str__()
+        return
+
+
+    def deleteVertex(self, vertex):
+        try:
+            del self.vertices[vertex]
+            for edges in self.vertices.values():
+                edges.remove(vertex)
+
+        except KeyError:
+            return
