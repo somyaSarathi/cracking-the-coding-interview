f1366f42baf5a8f17cf85a7e3661d57ea61c46fa
Somya Sarathi Samal <73218471+somyaSarathi@users.noreply.github.com>
2021-11-30 10:41:11 +0530

    Solved the problem 4.1 Route Between Nodes
    Approach:
        - dfs
        - bfs
    
    File Added: +1
        ./04_trees_and_graphs/01_route_between_nodes.js
    
    File Modified: 0

---
+++ 04_trees_and_graphs/01_route_between_nodes.js
@@ -0,0 +1,59 @@
+const prompt = require('prompt-sync')();
+
+const dfs = (graph, start, end, visited=new Set()) => {
+    if(start === end) return true;
+
+    if(visited.has(start)) return false;
+
+    visited.add(start)
+    let hasPath = false
+
+    for(let conn of graph[start]) {
+        hasPath ||= dfs(graph, conn, end, visited);
+    }
+
+    return hasPath
+}
+
+function bfs(graph, start, end) {
+    if(start === end) return true;
+
+    const visited = new Set()
+    const queue = [ start ]
+
+    while(queue.length > 0) {
+        let curr = queue.shift()
+        visited.add(curr)
+
+        for(let conn of graph[curr]) {
+            if(conn === end) return true;
+            if(!visited.has(conn)) queue.push(conn);
+            queue.push(conn)
+        }
+    }
+
+    return false;
+}
+
+/**
+    (f) → (g) → (h)
+     ↓  ↗
+    (i) ← (j)
+     ↓
+    (k)
+ */
+
+const graph = {
+    f: ['g', 'i'],
+    g: ['h'],
+    h: [],
+    i: ['g', 'k'],
+    j: ['i'],
+    k: [],
+};
+
+console.log(bfs(graph, 'f', 'k'));  // true
+console.log(bfs(graph, 'f', 'j'));  // false
+
+console.log(dfs(graph, 'i', 'h'));  // true
+console.log(dfs(graph, 'k', 'f'));  // false
