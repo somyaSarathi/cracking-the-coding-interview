72438619d7d0cd1447d75a273a10ab7967e24d2b
Somya Sarathi Samal <soms9edu@gmail.com>
2022-03-07 12:27:33 +0530

    Solved the problem 8.7 Permutation without dups

--- 08_recursion_and_dynamic_programming/07_permutation_without_dups.py
+++ 08_recursion_and_dynamic_programming/07_permutation_without_dups.py
@@ -2,14 +2,26 @@ import math
 import time
 
 
-def uniqStr():
-    pass
+# top-down
+def permute(string: str) -> list[str]:
+    # base case
+    if len(string) <= 1: return [ string ]
 
+    per = []
+    permutations = permute(string[:-1])
+    for permutation in permutations:
+        for  i in range(len(permutation)+1):
+            per.append(permutation[:i]+string[-1]+permutation[i:])
 
-if __name__ == '__main__':
+    return per
+ 
+
+if __name__ == '__main__': 
     START_TIME = time.time()
 
     # input
+    string = input().strip()
+    print(permute(string))
 
     print(f'program executed in {time.time() - START_TIME} second(s)')
     exit(0)
